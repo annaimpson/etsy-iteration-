524a9efda2ac8a0fc7c2d0d5ca1465e05b07a06d
annaimpson <annaimpson@gmail.com>
2016-02-18 15:56:09 -0500

    etsy-iteration

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function itemPriceAverage(){
     items.forEach(function(num){
       sellPrices += num.price;
   });
-  console.log(sellPrices/(items.length));
+  console.log("The average is: " + sellPrices/(items.length));
 
 }
 itemPriceAverage();
@@ -24,6 +24,13 @@ itemPriceAverage();
 //////Array of items/////
 ////between $14 & $18////
 /////////////////////////
+function itemsBetween(value){
 
+  return (value.price >= 14 && value.price <= 18);
+}
+console.log(items.filter(itemsBetween));
+/////////////////////////
+////////////////////////
+////////////////////////
 
 }());
